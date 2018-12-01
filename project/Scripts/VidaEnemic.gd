8f7eac5907937f5d1341a78b0f19dc58577a338e
FabulousDesu <owishplz@gmail.com>
2018-12-01 18:16:01 +0100

    11

--- project/Scripts/VidaEnemic.gd
+++ project/Scripts/VidaEnemic.gd
@@ -15,9 +15,9 @@ func update_barres():
 	$Cops.text = String(aux);
 	if _vida < 100:
 		$image_interior.modulate = (Color(0,0,0));
+	else:
+		$image_interior.modulate = (Color(255,0,0));
 
 func dibuixar(vida):
 	_vida = vida;
-	if _vida > 100:
-		$image_interior.modulate = (Color(255,0,0));
 	update_barres();
