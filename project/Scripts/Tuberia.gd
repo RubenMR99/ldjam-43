a31799cc791a1f49400277d9766c4379706a3816
RubenMR99 <ruben.moreno.romero@gmail.com>
2018-12-03 21:15:29 +0100

    v 12

--- project/Scripts/Tuberia.gd
+++ project/Scripts/Tuberia.gd
@@ -6,18 +6,19 @@ var temps = 0
 const temps_entre_generar = 200
 
 func _ready():
+	z_index = 20
 	var nom_propi = str(self.name)
 	if (nom_propi == "Tuberia_M"):
 		tipus = "MEAT"
-	elif("Tuberia_B"):
+	elif(nom_propi == "Tuberia_B"):
 		tipus = "BLOOD"
 	else:
 		tipus = "FAT"
 
 func _physics_process(delta):
-	temps = temps + 1
-	if (temps >= temps_entre_generar and GlobalVar.contador_personas < GlobalVar.MAX_PERSONAS):
-		temps = 0
+	var num_rand = randi() % 10000
+	print(num_rand)
+	if (num_rand < 20 and GlobalVar.contador_personas < GlobalVar.MAX_PERSONAS):
 		var pers_nova = PERSONA.instance()
 		pers_nova.asignar(tipus)
 		GlobalVar.contador_personas += 1
--- project/Scripts/monigote.gd
+++ project/Scripts/monigote.gd
@@ -8,6 +8,7 @@ var deixat_anar = false
 var entra_deixar = false
 var es_pot_agafar = false
 var morir = false
+var velocitat = 4
 
 var valor = 15;
 var tipus;
@@ -31,9 +32,14 @@ const p_4 = Vector2(896,0)
 func _ready():
 	var n_aleatori = randi() % 2
 	$Cap.frame = n_aleatori
+	z_index = -2
 
 func asignar(tipus_n):
 	tipus = tipus_n
+	if (tipus == "FAT"):
+		$Cap.set_texture(load("res://Sprites/Sacrificis/caps.png"))
+	else:
+		$Cos/Sprite.set_texture(load("res://Sprites/Sacrificis/MusculitosSpritesCuerpo.png"))
 
 func _on_KinematicBody2D_mouse_entered():
 	asobre = true
@@ -43,6 +49,12 @@ func _on_KinematicBody2D_mouse_entered():
 func _on_KinematicBody2D_mouse_exited():
 	if (not clicant):
 		asobre = false
+
+func _process(delta):
+	if (get_parent().stop and stage != 0):
+		movent = false
+	else:
+		movent = true
 
 func _physics_process(delta):
 	#rotation += 20
@@ -75,10 +87,10 @@ func _physics_process(delta):
 		$Cap.scale.y = lerp($Cap.scale.y, next_scale_cabezo.y, 0.2);
 		$Cap.scale.x = lerp($Cap.scale.x, next_scale_cabezo.x, 0.2);
 		
-		if (movent and !get_parent().stop):
+		if (movent):
 			position = guia(position)
 			pos_inicial = position
-		elif (!get_parent().stop):
+		else:
 			pos_inicial = guia(pos_inicial)
 	else:
 		scale.y = lerp(scale.y, 0, 0.2);
@@ -89,31 +101,37 @@ func _physics_process(delta):
 		
 		if (scale.y < 0.3):
 			queue_free()
-			get_parent().stop_sang()
+			#get_parent().stop_sang()
 
 func guia(posicio_actual):
 	if (posicio_actual.y < p_1.y and stage == 0):
-		posicio_actual.y += 3
+		posicio_actual.y += velocitat + 5
+		z_index = 1
 		if posicio_actual.y >= p_1.y:
 			stage = 1
 	elif (stage == 1):
-		posicio_actual.x -= 2
+		z_index = 2
+		posicio_actual.x -= velocitat
 		if posicio_actual.x <= p_2.x:
 			stage = 2
 	elif (stage == 2):
-		posicio_actual.x -= 2
-		posicio_actual.y += 2
+		posicio_actual.x -= velocitat
+		posicio_actual.y += velocitat
+		z_index = 3
 		if posicio_actual.y >= p_3.y:
 			stage = 3
 	elif (stage == 3):
-		posicio_actual.x += 2
+		z_index = 4
+		posicio_actual.x += velocitat
 		if (posicio_actual.x >= p_4.x):
 			stage = 4
 	elif (stage == 4):
 		get_parent().stop = true;
+		stage = 5
 	return posicio_actual
 
 func agafar():
+	z_index = 50
 	get_parent().agafar(self)
 	movent = false;
 	position = get_global_mouse_position()
@@ -139,19 +157,21 @@ func sortida_caldero():
 
 func eliminar():
 	if (tipus == "MEAT"):
-		GlobalVar.meat_rec += valor
-		print("MEAT")
-		print(GlobalVar.meat_rec)
+#		GlobalVar.meat_rec += valor
+#		print("MEAT")
+#		print(GlobalVar.meat_rec)
+		pass
 	elif (tipus == "BLOOD"):
-		GlobalVar.meat_rec += valor
+		GlobalVar.blood_rec += valor
 		print("BLOOD")
 		print(GlobalVar.blood_rec)
 	elif (tipus == "FAT"):
-		GlobalVar.meat_rec += valor
+		GlobalVar.fat_rec += valor
 		print("FAT")
 		print(GlobalVar.fat_rec)
 	GlobalVar.contador_personas -= 1
-	get_parent().stop = false;
+	if (stage == 5):
+		get_parent().stop = false;
 	get_parent().start_sang()
 	get_parent().deixar_anar()
 	morir = true
